9db713470a7bba60c0fdf37d13243c8779aab162
mitaone1000-rgb <mitaone1000@gmail.com>
2025-10-06 14:58:03 +0700

    Create PlaceData.lua

---
+++ PlaceData.lua
@@ -0,0 +1,9 @@
+-- PlaceData.lua (ModuleScript)
+-- Path: ServerScriptService/ModuleScript/PlaceData.lua
+
+local PlaceData = {
+	["Lobby"] = 101319079083908,
+	["ACT 1: Village"] = 91523772574713,
+}
+
+return PlaceData
