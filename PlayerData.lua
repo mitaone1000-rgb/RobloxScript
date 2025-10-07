82db67d9c4865edecca97ad3ded304b925fce6a1
mitaone1000-rgb <mitaone1000@gmail.com>
2025-10-08 00:18:40 +0700

    Update PlayerData.lua

--- PlayerData.lua
+++ PlayerData.lua
@@ -1,10 +1,10 @@
 -- PlayerData.lua (Script)
--- Path: ServerScriptService/PlayerData.lua
--- Deskripsi: Skrip ini menginisialisasi DataStoreManager untuk memulai penanganan data pemain.
+-- Path: ServerScriptService/Script/PlayerData.lua
+-- Script Place: Lobby & ACT 1: Village
 
 -- Memuat modul DataStoreManager agar sistemnya aktif
 local ServerScriptService = game:GetService("ServerScriptService")
-local DataStoreManager = require(ServerScriptService.DataStoreManager)
+local DataStoreManager = require(ServerScriptService.ModuleScript:WaitForChild("DataStoreManager"))
 
 -- Tidak ada logika lain yang diperlukan di sini, karena DataStoreManager
 -- sudah menangani semua event pemain secara internal.
