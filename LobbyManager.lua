274e16467324a6804bc6f2d26a1ec6d25c2789c6
mitaone1000-rgb <mitaone1000@gmail.com>
2025-10-07 23:25:31 +0700

    Update LobbyManager.lua

--- LobbyManager.lua
+++ LobbyManager.lua
@@ -5,4 +5,6 @@
 local ServerScriptService = game:GetService("ServerScriptService")
 
 -- Memanggil LevelManager agar event PlayerAdded-nya aktif di Lobby
+-- Memanggil modul-modul yang diperlukan agar event PlayerAdded-nya aktif di Lobby
 local LevelManager = require(ServerScriptService.ModuleScript:WaitForChild("LevelModule"))
+local CoinsManager = require(ServerScriptService.ModuleScript:WaitForChild("CoinsModule"))
