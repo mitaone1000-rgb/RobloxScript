0a93abf6a50e63347251cd61b0e7fcd00af8992d
mitaone1000-rgb <mitaone1000@gmail.com>
2025-10-11 15:55:42 +0700

    Update GachaConfig.lua

--- GachaConfig.lua
+++ GachaConfig.lua
@@ -1,18 +1,20 @@
 -- GachaConfig.lua (ModuleScript)
--- Path: ReplicatedStorage/ModuleScript/GachaConfig.lua
+-- Path: ServerScriptService/ModuleScript/GachaConfig.lua
 -- This module contains all the configuration settings for the gacha system.
 local GachaConfig = {
-    GACHA_COST = 100,
+	GACHA_COST = 100,
 
-    RARITY_CHANCES = {
-        Legendary = 5,  -- Chance in percent (5%)
-        Common = 95, -- Chance in percent (95%)
-    },
+	RARITY_CHANCES = {
+		Legendary = 5,  -- Chance in percent (5%)
+		Common = 95, -- Chance in percent (95%)
+	},
 
-    COMMON_REWARD_RANGE = {
-        Min = 10,
-        Max = 50
-    }
+	COMMON_REWARD_RANGE = {
+		Min = 10, 
+		Max = 50
+	},
+
+	PITY_THRESHOLD = 50 -- The number of rolls to guarantee a Legendary prize
 }
 
 return GachaConfig
